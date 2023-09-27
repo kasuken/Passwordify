f97b03903f4560deb3c967f0fa301196f1f4d324
Defkon1 <defkon1@gmail.com>
2023-09-27 10:32:42 +0200

    Added i18n support

---
+++ Localizer/LocalizerHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Passwordify.Localizer
+{
+    public static class LocalizerHelper
+    {
+        public const string NeutralCulture = "en-US";
+
+        public static readonly CultureInfo[] SupportedCultures = new CultureInfo[]
+        {
+            new CultureInfo(NeutralCulture),
+            new CultureInfo("it-IT")
+        };
+
+        public static bool IsSupported(string culture)
+        {
+            return SupportedCultures.Any(c => string.Equals(c.Name, culture, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
